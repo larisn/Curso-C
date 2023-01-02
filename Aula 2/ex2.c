8f0718154272c8b1ec61a0ece791a23a6c4cddba
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 15:33:59 -0300

    Create ex2.c

---
+++ Aula 2/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	
+	int idade = 0;
+	
+	printf("O valor inicial da sua idade: %d.\n", idade);
+	
+	printf("Digite uma idade:\n");
+	scanf("%d", &idade);
+	
+	printf("Idade informada: %d\n", idade);
+
+system ("pause");
+}
