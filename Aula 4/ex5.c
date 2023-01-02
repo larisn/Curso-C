ee7fe3459caccf08a6575146230edfcfaa86161f
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 16:27:25 -0300

    Create ex5.c

---
+++ Aula 4/ex5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+
+float v1, v2;
+
+
+	printf("Digite o primeiro valor:\n");
+	scanf("%f", &v1);
+
+	printf("Digite o segundo valor:\n");
+	scanf("%f", &v2);
+
+	if (v1 > v2){
+		printf("O maior valor é: %.3f.\n", v1);
+
+	}else {
+		printf("O maior valor é %.3f.\n", v2);
+	}
+}
+
+	
+
+
+
+
+
