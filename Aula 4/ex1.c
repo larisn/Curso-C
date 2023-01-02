d51c53d3c36c1b39b51e110eb986664c1ecd85b9
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 16:25:07 -0300

    Update ex1.c

--- Aula 4/ex1.c
+++ Aula 4/ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+# define texto "Entrada e saída de dados"
 
+int main(){
+
+    float m;
+
+printf("Insira a nota: \n");
+scanf("%f", &m);
+
+    if (m>= 7.0){
+
+        printf("Aprovado(a)!\n");
+
+    } else {
+        printf("Reprovado(a)!\n");
+    }
+
+}    
