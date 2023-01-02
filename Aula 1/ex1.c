f7130a4dd4b558182aeabc8b0e5005fd5a6876d3
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 15:32:50 -0300

    Create ex1.c

---
+++ Aula 1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    int idade, idade2;
+
+    printf("Digite sua idade aqui:\n");
+    scanf("%d", &idade);
+
+    idade2 = idade + 1;
+    printf("Ano que vem você terá %d anos."); idade2;
+}
