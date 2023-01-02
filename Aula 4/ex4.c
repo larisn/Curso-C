96041d5aeeaebcc81ffed4993d8f6996a955bfaa
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 16:27:04 -0300

    Create ex4.c

---
+++ Aula 4/ex4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main(){
+
+int senha;
+
+printf("Digite uma senha:\n");
+scanf("%d", &senha);
+
+	if (senha == 1234){
+		printf("ACESSO PERMITIDO\n");
+	}
+	
+	else {
+		printf("ACESSO NEGADO\n");
+		
+	}
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
