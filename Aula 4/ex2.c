1b31b81058d60fb55679461d1c863616d24eabb1
Larissa <121525880+larisn@users.noreply.github.com>
2023-01-02 16:25:25 -0300

    Create ex2.c

---
+++ Aula 4/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+# define texto "Entrada e saída de dados"
+
+int main(){
+
+    float m;
+
+printf("Insira a nota: \n");
+scanf("%f", &m);
+
+    if (m>= 4.0 && m <7.0){
+
+        printf("Tem direito a exame!\n");
+
+}
+
+}    
